9564cfa30f8360fd16c2dfb0280c182f6c2d8d71
Howard Chang <howard3433@gmail.com>
2016-04-06 17:45:41 +0800

    Complete question 14 & 15

--- hw4/main.js
+++ hw4/main.js
@@ -19,3 +19,44 @@ let { w, errRate } = LinearRegressionTrain(trainingData, 10);
 Ein = errRate;
 EOut = zeroOneError(testData, w);
 console.log(`Q13: With lambda set to 10, Ein is ${Ein} and Eout is ${EOut}`);
+
+//Q14 & 15
+let lamdaList = [],
+    minEinResult = {
+        lambda: undefined,
+        minEin: Infinity,
+        Eout: undefined
+    },
+    minEoutResult = {
+        lambda: undefined,
+        Ein: undefined,
+        minEout: Infinity
+    };
+
+for (let i = 2; i > -11; i--) {
+    lamdaList.push(Math.pow(10, i));
+}
+
+minEinResult = lamdaList.reduce((result, lambda) => {
+    let temp = LinearRegressionTrain(trainingData, lambda);
+    let tempEOut = zeroOneError(testData, temp.w);
+    return (temp.errRate < result.minEin) ? {
+        lambda: Math.log(lambda) / Math.log(10),
+        minEin: temp.errRate,
+        Eout: tempEOut
+    } : result;
+}, minEinResult);
+
+console.log(`Q14: Minimum Ein is achieved by log(lambda) = ${minEinResult.lambda} and Ein is ${minEinResult.minEin} with Eout equal to ${minEinResult.Eout}`);
+
+minEoutResult = lamdaList.reduce((result, lambda) => {
+    let temp = LinearRegressionTrain(trainingData, lambda);
+    let tempEOut = zeroOneError(testData, temp.w);
+    return (tempEOut < result.minEout) ? {
+        lambda: Math.log(lambda) / Math.log(10),
+        Ein: temp.errRate,
+        minEout: tempEOut
+    } : result;
+}, minEoutResult);
+
+console.log(`Q15: Minimum Eout is achieved by log(lambda) = ${minEoutResult.lambda} and Ein is ${minEoutResult.Ein} with Eout equal to ${minEoutResult.minEout}`);
